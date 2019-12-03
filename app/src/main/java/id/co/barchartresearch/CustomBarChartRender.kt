c2cf994f4d65e30986ab08fd580eaeaf89f645cf
Irfan Pertadima <pertadimairfan@gmail.com>
2019-12-03 16:00:50 +0700

    [irfan] custom rounded

---
+++ app/src/main/java/id/co/barchartresearch/CustomBarChartRender.kt
@@ -0,0 +1,234 @@
+package id.co.barchartresearch
+
+import android.graphics.*
+import com.github.mikephil.charting.animation.ChartAnimator
+import com.github.mikephil.charting.interfaces.dataprovider.BarDataProvider
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
+import com.github.mikephil.charting.renderer.BarChartRenderer
+import com.github.mikephil.charting.utils.Utils
+import com.github.mikephil.charting.utils.ViewPortHandler
+import kotlin.math.ceil
+
+/**
+ * Created by pertadima on 03,December,2019
+ */
+
+class CustomBarChartRender(
+    chart: BarDataProvider?,
+    animator: ChartAnimator?,
+    viewPortHandler: ViewPortHandler?
+) :
+    BarChartRenderer(chart, animator, viewPortHandler) {
+    private val barShadowRectBuffer = RectF()
+    private var barRadius = 0
+
+    fun setRadius(mRadius: Int) {
+        this.barRadius = mRadius
+    }
+
+    //SOME HUNGARIAN NOTATION COME FROM LIBRARY
+    override fun drawDataSet(canvas: Canvas, dataSet: IBarDataSet, index: Int) {
+        val trans = mChart.getTransformer(dataSet.axisDependency)
+
+        mBarBorderPaint.apply {
+            color = dataSet.barBorderColor
+            strokeWidth = Utils.convertDpToPixel(dataSet.barBorderWidth)
+        }
+        mShadowPaint.color = dataSet.barShadowColor
+
+        val drawBorder = dataSet.barBorderWidth > 0f
+        val phaseX = mAnimator.phaseX
+        val phaseY = mAnimator.phaseY
+
+        if (mChart.isDrawBarShadowEnabled) {
+            mShadowPaint.color = dataSet.barShadowColor
+            val barData = mChart.barData
+            val barWidth = barData.barWidth
+            val barWidthHalf = barWidth / 2.0f
+            var x: Float
+            var i = 0
+            val count =
+                ceil((dataSet.entryCount.toFloat() * phaseX).toDouble()).coerceAtMost(dataSet.entryCount.toDouble())
+
+            while (i < count) {
+                val barEntry = dataSet.getEntryForIndex(i)
+                x = barEntry.x
+
+                barShadowRectBuffer.apply {
+                    left = x - barWidthHalf
+                    right = x + barWidthHalf
+                }
+                trans.rectValueToPixel(barShadowRectBuffer)
+
+                if (mViewPortHandler.isInBoundsLeft(barShadowRectBuffer.right).not()) {
+                    i++
+                    continue
+                }
+
+                if (mViewPortHandler.isInBoundsRight(barShadowRectBuffer.left).not()) break
+                barShadowRectBuffer.apply {
+                    top = mViewPortHandler.contentTop()
+                    bottom = mViewPortHandler.contentBottom()
+                }
+
+                canvas.drawRoundRect(
+                    mBarRect,
+                    barRadius.toFloat(),
+                    barRadius.toFloat(),
+                    mShadowPaint
+                )
+                i++
+            }
+        }
+        // initialize the buffer
+        val barBuffer = mBarBuffers[index].apply {
+            setPhases(phaseX, phaseY)
+            setDataSet(index)
+            setInverted(mChart.isInverted(dataSet.axisDependency))
+            setBarWidth(mChart.barData.barWidth)
+            feed(dataSet)
+        }
+
+        trans.pointValuesToPixel(barBuffer.buffer)
+        val isSingleColor = dataSet.colors.size == 1
+        if (isSingleColor) {
+            mRenderPaint.color = dataSet.color
+        }
+        var j = 0
+        while (j < barBuffer.size()) {
+            if (mViewPortHandler.isInBoundsLeft(barBuffer.buffer[j + 2]).not()) {
+                j += 4
+                continue
+            }
+            if (mViewPortHandler.isInBoundsRight(barBuffer.buffer[j]).not()) break
+
+            if (isSingleColor.not()) { // Set the color for the currently drawn value. If the index | is out of bounds, reuse colors.
+                mRenderPaint.color = dataSet.getColor(j / 4)
+            }
+
+            if (dataSet.gradientColor != null) {
+                val gradientColor = dataSet.gradientColor
+                with(barBuffer) {
+                    mRenderPaint.shader = LinearGradient(
+                        buffer[j],
+                        buffer[j + 3],
+                        buffer[j],
+                        buffer[j + 1],
+                        gradientColor.startColor,
+                        gradientColor.endColor,
+                        Shader.TileMode.MIRROR
+                    )
+                }
+            }
+            if (dataSet.gradientColors != null) {
+                with(barBuffer) {
+                    mRenderPaint.shader = LinearGradient(
+                        buffer[j],
+                        buffer[j + 3],
+                        buffer[j],
+                        buffer[j + 1],
+                        dataSet.getGradientColor(j / 4).startColor,
+                        dataSet.getGradientColor(j / 4).endColor,
+                        Shader.TileMode.MIRROR
+                    )
+                }
+            }
+
+            with(barBuffer) {
+                val path2: Path = roundRect(
+                    RectF(buffer[j], buffer[j + 1], buffer[j + 2], buffer[j + 3]),
+                    barRadius.toFloat(),
+                    barRadius.toFloat(),
+                    tl = true,
+                    tr = true,
+                    br = false,
+                    bl = false
+                )
+                canvas.drawPath(path2, mRenderPaint)
+            }
+
+            if (drawBorder) {
+                with(barBuffer) {
+                    val path: Path = roundRect(
+                        RectF(buffer[j], buffer[j + 1], buffer[j + 2], buffer[j + 3]),
+                        barRadius.toFloat(),
+                        barRadius.toFloat(),
+                        tl = true,
+                        tr = true,
+                        br = false,
+                        bl = false
+                    )
+                    canvas.drawPath(path, mBarBorderPaint)
+                }
+            }
+            j += 4
+        }
+    }
+
+    private fun roundRect(
+        rect: RectF,
+        rx: Float,
+        ry: Float,
+        tl: Boolean,
+        tr: Boolean,
+        br: Boolean,
+        bl: Boolean
+    ): Path {
+        var rx = rx
+        var ry = ry
+        val top = rect.top
+        val left = rect.left
+        val right = rect.right
+        val bottom = rect.bottom
+
+        if (rx < 0) rx = DEFAULT_VALUE
+        if (ry < 0) ry = DEFAULT_VALUE
+
+        val width = right - left
+        val height = bottom - top
+
+        if (rx > width / 2) rx = width / 2
+        if (ry > height / 2) ry = height / 2
+
+        val widthMinusCorners = width - 2 * rx
+        val heightMinusCorners = height - 2 * ry
+
+        return Path().apply {
+            moveTo(right, top + ry)
+
+            if (tr) rQuadTo(DEFAULT_VALUE, -ry, -rx, -ry) //top-right corner
+            else {
+                rLineTo(DEFAULT_VALUE, -ry)
+                rLineTo(-rx, DEFAULT_VALUE)
+            }
+            rLineTo(-widthMinusCorners, DEFAULT_VALUE)
+
+            if (tl) rQuadTo(-rx, DEFAULT_VALUE, -rx, ry) //top-left corner
+            else {
+                rLineTo(-rx, DEFAULT_VALUE)
+                rLineTo(DEFAULT_VALUE, ry)
+            }
+            rLineTo(DEFAULT_VALUE, heightMinusCorners)
+
+            if (bl) rQuadTo(DEFAULT_VALUE, ry, rx, ry) //bottom-left corner
+            else {
+                rLineTo(DEFAULT_VALUE, ry)
+                rLineTo(rx, DEFAULT_VALUE)
+            }
+            rLineTo(widthMinusCorners, DEFAULT_VALUE)
+
+            if (br) rQuadTo(rx, DEFAULT_VALUE, rx, -ry) //bottom-right corner
+            else {
+                rLineTo(rx, DEFAULT_VALUE)
+                rLineTo(0F, -ry)
+            }
+            rLineTo(DEFAULT_VALUE, -heightMinusCorners)
+
+            close() //Given close, last lineto can be removed.
+        }
+    }
+
+    companion object {
+        private const val DEFAULT_VALUE = 0F
+    }
+}
--- app/src/main/java/id/co/barchartresearch/MainActivity.kt
+++ app/src/main/java/id/co/barchartresearch/MainActivity.kt
@@ -1,12 +1,145 @@
 package id.co.barchartresearch
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.RectF
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.github.mikephil.charting.components.XAxis.XAxisPosition
+import com.github.mikephil.charting.components.YAxis.AxisDependency
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.formatter.ValueFormatter
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener
+import com.github.mikephil.charting.model.GradientColor
+import com.github.mikephil.charting.utils.MPPointF
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val listData by lazy {
+        mutableListOf(
+            ChartData("1/9", 60.5F),
+            ChartData("2/9", 72.5F),
+            ChartData("3/9", 80.75F),
+            ChartData("4/9", 67.2F),
+            ChartData("5/9", 88.5F),
+            ChartData("6/9", 85.3F),
+            ChartData("7/9", 70.5F)
+        )
+    }
+
+    private val startColor by lazy {
+        ContextCompat.getColor(this, android.R.color.holo_orange_dark)
+    }
+    private val endColor by lazy {
+        ContextCompat.getColor(this, android.R.color.holo_orange_light)
+    }
+
+    private val whiteColor by lazy {
+        ContextCompat.getColor(this, R.color.colorWhite)
+    }
+
+    private val transparentWhiteColor by lazy {
+        ContextCompat.getColor(this, R.color.colorWhiteTransparent)
+    }
+
+    private val barGradientColor by lazy {
+        mutableListOf(GradientColor(startColor, endColor))
+    }
+
+    private val onValueSelectedRectF by lazy {
+        RectF()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initChart()
+    }
+
+    private fun initChart() {
+        with(bar_chart) {
+            description.isEnabled = false
+            legend.isEnabled = false
+            isDoubleTapToZoomEnabled = false
+
+            setPinchZoom(false)
+            setDrawBarShadow(false)
+            setDrawValueAboveBar(false)
+            animateY(1500)
+
+            xAxis.apply {
+                position = XAxisPosition.BOTTOM
+                setDrawGridLines(false)
+                textColor = whiteColor
+                valueFormatter = object : ValueFormatter() {
+                    override fun getFormattedValue(value: Float): String {
+                        return listData[0].date
+                    }
+                }
+            }
+
+            axisRight.apply {
+                textColor = whiteColor
+                setDrawAxisLine(false)
+                gridColor = transparentWhiteColor
+                axisMinimum = 0F
+                axisMaximum = 100F
+            }
+            axisLeft.apply {
+                isEnabled = false
+                gridColor = transparentWhiteColor
+            }
+
+            val barChartRender = CustomBarChartRender(this, animator, viewPortHandler).apply {
+                setRadius(50)
+            }
+            renderer = barChartRender
+
+            setOnChartValueSelectedListener(object : OnChartValueSelectedListener {
+                override fun onNothingSelected() {
+
+                }
+
+                override fun onValueSelected(e: Entry?, h: Highlight?) {
+                    if (e == null) return
+
+                    val bounds: RectF = onValueSelectedRectF
+                    bar_chart.getBarBounds(e as BarEntry?, bounds)
+                    val position: MPPointF = bar_chart.getPosition(e, AxisDependency.LEFT)
+
+                    MPPointF.recycleInstance(position)
+                }
+            })
+        }
+        setData()
+    }
+
+    private fun setData() {
+        val values = mutableListOf<BarEntry>()
+        var index = 1
+        listData.forEach {
+            index++
+            values.add(BarEntry(index.toFloat(), it.value))
+        }
+
+        val barDataSet = BarDataSet(values, "").apply {
+            setDrawIcons(false)
+            setDrawValues(false)
+            gradientColors = barGradientColor
+        }
+
+        val dataSets = mutableListOf(barDataSet)
+        val data = BarData(dataSets as List<IBarDataSet>?).apply {
+            setValueTextSize(10F)
+            barWidth = 0.6F
+        }
+
+        bar_chart.data = data
     }
 }
